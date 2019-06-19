51b29183d72339084dbaedfad886a65f442c0570
Evgen Markelov <noach4se@gmail.com>
2019-06-20 00:13:06 +0200

    promise writed

--- script.js
+++ script.js
@@ -1,66 +1,71 @@
-const searchForm = document.querySelector('#search-form');
-const movie = document.querySelector('#movies');
+const searchForm = document.querySelector("#search-form");
+const movie = document.querySelector("#movies");
 
-function apiSearch(event){
-    event.preventDefault();
-    const searchText = document.querySelector('.form-control').value, 
-        server = 'https://api.themoviedb.org/3/search/multi?api_key=3c6b5a6fe41eedd4960be722f6bc85b7&language=ru&query=' + searchText;
+function apiSearch(event) {
+  event.preventDefault();
+  const searchText = document.querySelector(".form-control").value,
+    server = "https://api.themoviedb.org/3/search/multi?api_key=3c6b5a6fe41eedd4960be722f6bc85b7&language=ru&query=" + searchText;
+    movie.innerHTML = "Loading...";
+    requestApi("GET", server).then(function (result) {
+      const output = JSON.parse(result);
+      console.log(output);
 
-    requestApi('GET', server);
+      let inner = "";
+
+      output.results.forEach(function(item) {
+        let nameItem = item.name || item.title;
+        let itemDate = item.first_air_date || item.release_date;
+
+        console.log(nameItem);
+        inner += `<div class="col-12 col-md-4 col-xl-3">${nameItem}<br>Release Date: ${itemDate}</div>`;
+      });
+      movie.innerHTML = inner;
+    })
+    .catch(function(reason){
+        movie.innerHTML = 'Oopsee something just went wrong..!';
+        console.log('error: ' + reason.status);
+    });
 }
 // apiSearch('hi');
-searchForm.addEventListener('submit', apiSearch);
+searchForm.addEventListener("submit", apiSearch);
 
-function requestApi(method, url){
-
+function requestApi(method, url) {
+  return new Promise(function(resolve, reject) {
     const request = new XMLHttpRequest();
-    // console.log(request);
     request.open(method, url);
-    request.send();
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState !== 4) return;
-        if (request.status !== 200){
-            console.log('error: ' + request.status);
-            return;
-        }
-        const output = JSON.parse(request.responseText);
 
-        let inner = '';
-
-        // output.results.forEach(function (item, itemDate){
-        //     let nameItem = (item.name + item.first_air_date) || (item.title + item.release_date);
-        //     console.log(nameItem);
-        //     inner += `<div class="col-12 col-md-4 col-xl-3">${nameItem}</div>`;
-        // });
-        // movie.innerHTML = inner;
-        // console.log(output);
-        output.results.forEach(function (item){
-            let nameItem = item.name || item.title;
-            let itemDate = item.first_air_date || item.release_date;
-        
-            console.log(nameItem);
-            inner += `<div class="col-12 col-md-4 col-xl-3">${nameItem}<br>Release Date: ${itemDate}</div>`;
+    request.addEventListener("load", function() {
+      if (request.status !== 200) {
+        reject({
+          status: request.status
         });
-        movie.innerHTML = inner;
-        console.log(output);
+        return;
+      }
 
-        // output.results.forEach(function (item){
-        //     let nameItem = item.name || item.title;
-        //     console.log(nameItem);
-        //     inner += `<div class="col-12 col-md-4 col-xl-3">${nameItem}</div>`;
-        // });
-        // movie.innerHTML = inner;
-        // console.log(output);
-
-        // output.results.forEach(function (itemDate){
-        //     let nameItemDate = itemDate.first_air_date || itemDate.release_date;
-        //     console.log(nameItemDate);
-        //     inner += `<div class="col-12 col-md-4 col-xl-3">${nameItemDate}</div>`;
-        // });
-        // movie.innerHTML = inner;
-        // console.log(output);
-
+      resolve(request.response);
     });
 
-    // return url;
+    request.addEventListener("error", function() {
+      reject({
+        status: request.status
+      });
+    });
+    request.send();
+  });
+
+  // console.log(request);
+
+  // request.addEventListener('readystatechange', () => {
+  //     if (request.readyState !== 4) return;
+  //     movie.innerHTML = 'Loading...';
+  //     if (request.status !== 200){
+  //         movie.innerHTML = 'Something went wrong';
+  //         console.log('error: ' + request.status);
+  //         return;
+  //     }
+
+  //     console.log(output);
+  // });
+
+  // return url;
 }
