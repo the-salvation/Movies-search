ba0d4dbec4ece92dd79496de179e1bd82f5feecf
Evgen Markelov <noach4se@gmail.com>
2019-06-20 01:10:32 +0200

    posters added and some styling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const searchForm = document.querySelector("#search-form");
 const movie = document.querySelector("#movies");
+const urlPoster = 'https://image.tmdb.org/t/p/w500/';
 
 function apiSearch(event) {
   event.preventDefault();
@@ -10,20 +11,27 @@ function apiSearch(event) {
   movie.innerHTML = "Loading...";
   fetch(server)
     .then(function(value) {
+      if (value.status !== 200) {
+        return Promise.reject(value.status);
+      } 
       return value.json();
     })
     .then(function(output) {
+      console.log(output);
       let inner = "";
-      output.results.forEach(function(item) {
+      output.results.forEach(function (item) {
         let nameItem = item.name || item.title;
         let itemDate = item.first_air_date || item.release_date;
-        inner += `<div class="col-12 col-md-4 col-xl-3">${nameItem}<br>Release Date: ${itemDate}</div>`;
+        inner += `<div class="col-12 col-md-4 col-xl-3 item">
+        <img src="${urlPoster + item.poster_path}" alt="${nameItem}">
+        <h5>${nameItem}</h5><br>Release Date: <b>${itemDate}</b>
+        </div>`;
       });
       movie.innerHTML = inner;
     })
     .catch(function(reason){
       movie.innerHTML = "Oopsee something just went wrong..!";
-      console.log("error: " + reason.status);
+      console.error("error: " + reason.status);
     });
   }
 searchForm.addEventListener("submit", apiSearch);
